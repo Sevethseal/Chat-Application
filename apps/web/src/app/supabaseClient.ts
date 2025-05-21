c8d2bf09fd617100e2d9429e9edebf9fa69dd356
Sevethseal <kamura657@gmail.com>
2025-05-22 01:14:11 +0200

    fix security issue

--- apps/web/src/app/supabaseClient.ts
+++ apps/web/src/app/supabaseClient.ts
@@ -1,7 +1,8 @@
+// lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mobhgsgpovvqbrwteoiv.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYmhnc2dwb3Z2cWJyd3Rlb2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NDgxMjUsImV4cCI6MjA2MzQyNDEyNX0.WNzWL4lcTL9coCVnNGBK7Xll_WUrJ5TlVbNSr88mCi8";
+// These env vars need to be set in .env.local (and in Vercel Dashboard)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
